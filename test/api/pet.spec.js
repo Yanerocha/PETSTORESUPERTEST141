5406710b42b6e1f03e0573a88552241d6901369a
Yanerocha <yanerocha22@outlook.com>
2024-08-06 21:36:06 -0300

    first commit

---
+++ test/api/pet.spec.js
@@ -0,0 +1,24 @@
+// Bibliotecas e Framework
+const supertest = require('supertest')
+
+const petId = 559304501
+
+// Em JavaScript, Classe é opcional, mas pode agrupar em um Describe
+describe('Api PetStore Swagger - Entidade Pet', () =>{
+
+    // Atributos do grupo/describe
+    const request = supertest('https://petstore.swagger.io/v2') // BaseURL
+
+    // Funções e Métodos: Its
+    it('POST Pet', () => {
+
+    // Atributos, Campos, Características, Configurações
+
+    // Funções de Apoio (Opcional)
+
+    // Funções de Teste em Si
+
+    
+}) // Final do método POST
+
+}) // termina a describe
